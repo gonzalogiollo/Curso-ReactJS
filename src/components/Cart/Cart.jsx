757ec54b9e6e0be9d1e96fc2686862d12c67c7ae
Jonathan-Gonzalo-Giollo <Jonathan-Gonzalo-Giollo@kyndryl.com>
2022-11-22 12:58:23 -0300

    ultimos detalles en checkout

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,29 +9,38 @@ const Cart = () => {
     const {cart, deleteAll, deleteOne, totalPrecio} = useContext(CartContext) 
     if(cart.length === 0) return <h1>Aún no hay productos, volver al <Link to='/'>Inicio</Link></h1>
     return (
-        <div>
+        <>
+        <div className="container-checkout">
             {cart.map((prod) =>(
                 <div>
-                    <img src={prod.img} alt={prod.title} width='150px'/>
 
-                    <div>
+                    <div  className="img-container">
+                     <img className="img"  src={prod.img} alt={prod.title} width='150px'/>
+                    </div>
+                    
+
+                    <div className='info-detalle-checkout'>
                         <h3>{prod.title}</h3>
-                        <h3>{prod.count}</h3>
+                        <h3>{prod.price}</h3>
                         <button onClick={()=> deleteOne(prod.id)}>Eliminar producto </button>
                     </div>
 
                     
                     
+                    
+                            
                 </div>
   
             ))}
 
-            <h2> Total: $ {totalPrecio()}</h2>
-            <button onClick={deleteAll}>Vaciar carrito </button>
-            <button><Link to='/checkout'>checkout</Link></button>
-
         </div>
-     
+        <div className='info-checkout'>
+             <h2> Total: $ {totalPrecio()}</h2>
+             <button className="btn-detalle" onClick={deleteAll}>Vaciar carrito </button>
+             <button className="btn-detalle"><Link to='/checkout'>Finalizar compra</Link></button>
+           </div>
+        </>
+        
     );
 };
 
